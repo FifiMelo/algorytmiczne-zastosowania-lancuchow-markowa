d1dbe8300d37ee52ef6d4f7a4c4b9cb15c8ea2b1
FifiMelo <felipe336336@gmail.com>
2024-05-23 13:05:53 +0200

    added __call__ function

--- permutation.py
+++ permutation.py
@@ -13,10 +13,10 @@ class Permutation:
         out = []
         if self.n == len(Permutation.chars):
             for i in range(self.n):
-                out.append(f"{Permutation.chars[i]} -> {Permutation.chars[self.indexes[i]]}")
+                out.append(f"{Permutation.chars[i]} -> {Permutation.chars[self(i)]}")
         else:
             for i in range(self.n):
-                out.append(f"{i} -> {self.indexes[i]}")
+                out.append(f"{i} -> {self(i)}")
         return "\n".join(out)
     
     def __repr__(self) -> str:
@@ -24,17 +24,20 @@ class Permutation:
     
     def __mul__(self, other: 'Permutation') -> 'Permutation':
         assert self.n == other.n
-        return Permutation([self.indexes[other.indexes[i]] for i in range(self.n)])
+        return Permutation([self(other(i)) for i in range(self.n)])
 
     def inverse(self) -> 'Permutation':
         out = [0 for i in range(self.n)]
         for i in range(self.n):
-            out[self.indexes[i]] = i
+            out[self(i)] = i
         return Permutation(out)
     
     def modify(self, index1: int, index2: int) -> None:
         self.indexes[index1] = index2
         self.indexes[index2] = index1
+    
+    def __call__(self, k: int)->int:
+        return self.indexes[k]
     
 def create_identity(n: int) -> Permutation:
     return Permutation([i for i in range(n)])
